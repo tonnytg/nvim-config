a7bc7680b3c35c6ebfd64a54c9486baaccd15946
Antonio Thomacelli Gomes <tonnytg@gmail.com>
2025-04-05 22:43:35 -0300

    feat: add tab to add space

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -11,3 +11,15 @@ vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 -- Busca por algo dentro dos arquivos
 vim.keymap.set('n', '<C-f>', "<cmd>Telescope live_grep<cr>", { noremap = true, silent = true })
 
+-- Modo normal: adiciona indentação na linha atual
+vim.keymap.set("n", "<Tab>", ">>", { noremap = true, silent = true })
+
+-- Modo visual: adiciona indentação em todas as linhas selecionadas
+vim.keymap.set("v", "<Tab>", ">gv", { noremap = true, silent = true })
+
+-- Modo visual: remove indentação com Shift+Tab
+vim.keymap.set("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
+
+-- Modo normal: remove indentação na linha atual com Shift+Tab
+vim.keymap.set("n", "<S-Tab>", "<<", { noremap = true, silent = true })
+
